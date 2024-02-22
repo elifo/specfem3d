3510bf8fea247f5a2c28b44498e7d0906aadace9
Elif Oral <elifo@ch-fe2.lanl.gov>
2024-02-22 10:53:11 -0700

    update config file for LANL

--- ELIF_config_and_compile.sh
+++ ELIF_config_and_compile.sh
@@ -1,10 +1,12 @@
-# I use this repo to debug big-mesh problem 
-# when using dynamic rupture for Ridgecrest and
-# Hetero paper (07/22)
-
+# caltech cluster config
 # module load cuda/11.2 openmpi/4.1.1_cuda-11.2
+#./configure FC=gfortran MPIFC=mpif90  --with-cuda=cuda8
 
-./configure FC=gfortran MPIFC=mpif90  --with-cuda=cuda8
+# modules to load
+# cray-mpich/8.1.26  cuda/11.6
+
+# Carene's for LANL Chicoma
+./configure FC=ftn MPIFC=ftn CC=cc CXX=CC --with-cuda=cuda11 CUDA_INC="$CUDA_INCLUDES" CUDA_LIB="$CUDA_LIBS" MPI_INC=$MPICH_DIR/include
 
 make clean 
 make all
